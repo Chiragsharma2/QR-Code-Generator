43192a53146a08351ca4d71a56ebd9816ecf3325
Chirag <90318064+Chiragsharma2@users.noreply.github.com>
2024-06-21 14:34:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import inquirer from "inquirer";
+/* 
+1. Use the inquirer npm package to get userinput.
+ 
+
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
